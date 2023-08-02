cf8792dcb4585bc5a0cfd48989d81fc9fa9cd9ce
DESKTOP-QKIBCGM\KJHH <2687486793@qq.com>
2023-08-02 15:04:54 +0800

    引入tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
