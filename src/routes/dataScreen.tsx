2c3d566a1eb9241a3cc2b7e244309d65b743a0c3
DESKTOP-QKIBCGM\KJHH <2687486793@qq.com>
2023-08-26 17:16:36 +0800

    测试2

--- src/routes/dataScreen.tsx
+++ src/routes/dataScreen.tsx
@@ -1,5 +1,5 @@
 import { PageContainer } from '@ant-design/pro-layout';
 
 export default function DataScreen() {
-  return <PageContainer>qooqooooo</PageContainer>;
+  return <PageContainer>啦啦啦</PageContainer>;
 }
